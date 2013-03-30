18a6ddbb7cc7f20a1691291e230860ecf9039cab
agabrown <agabrown@xs4all.nl>
2013-03-30 10:38:10 +0100

    version in splashscreen fixed

--- src/agabrown/fractalexplorer/util/FEConstants.java
+++ src/agabrown/fractalexplorer/util/FEConstants.java
@@ -34,7 +34,7 @@ public final class FEConstants {
   /**
    * Package version.
    */
-  public static final String VERSION = "0.3.1";
+  public static final String VERSION = "0.3.2";
 
   /**
    * The constant 1/2;
